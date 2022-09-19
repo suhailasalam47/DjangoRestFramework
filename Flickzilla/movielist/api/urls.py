a3d34645ae78cdca468bae64b98fabcd303d112e
suhaila <suhailasalam47@gmail.com>
2022-09-19 18:36:09 +0530

    Adding serializers

--- Flickzilla/movielist/api/urls.py
+++ Flickzilla/movielist/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from movielist.api import views
 
 
 urlpatterns = [
---
+++ Flickzilla/movielist/api/views.py
@@ -0,0 +1,17 @@
+from movielist.models import Movie
+from .serializers import MovieSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)

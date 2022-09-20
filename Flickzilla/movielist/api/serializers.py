131b4a33981a5b03a5c80a2589803767e807d4c4
suhaila <suhailasalam47@gmail.com>
2022-09-20 10:55:23 +0530

    Added a custom serializer field

--- Flickzilla/movielist/api/serializers.py
+++ Flickzilla/movielist/api/serializers.py
@@ -1,30 +1,49 @@
+from dataclasses import field
 from rest_framework import serializers
 from movielist.models import Movie
 
 
-def validate_name( value):
-        if len(value)<3:
-            raise serializers.ValidationError("Name must have atleast 3 characters")
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
 
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # exclude = ['active']
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[validate_name])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_len_name(self, object):
+        return len(object.name)
+        
         
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError("name and description should not be same")
         else:
             return data
+
+# def validate_name( value):
+#         if len(value)<3:
+#             raise serializers.ValidationError("Name must have atleast 3 characters")
+
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[validate_name])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.active = validated_data.get('active', instance.active)
+    #     instance.save()
+    #     return instance
+        
+    # def validate(self, data):
+    #     if data['name'] == data['description']:
+    #         raise serializers.ValidationError("name and description should not be same")
+    #     else:
+    #         return data

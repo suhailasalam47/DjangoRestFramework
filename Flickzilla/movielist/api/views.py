538ccbb8185fa863544430e264ed42d9e2ff5cd3
suhaila <suhailasalam47@gmail.com>
2022-09-21 14:21:09 +0530

    Concrete view implemented

--- Flickzilla/movielist/api/views.py
+++ Flickzilla/movielist/api/views.py
@@ -7,23 +7,32 @@ from rest_framework.views import APIView
 from rest_framework import mixins, generics
 
 
-class ReviewDetails(mixins.RetrieveModelMixin, generics.GenericAPIView):
+class ReviewList(generics.ListCreateAPIView):
     queryset = Reviews.objects.all()
     serializer_class = ReviewsSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-        
 
-class ReviewList(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
+class ReviewDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Reviews.objects.all()
     serializer_class = ReviewsSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+# class ReviewDetails(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Reviews.objects.all()
+#     serializer_class = ReviewsSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+
+# class ReviewList(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
+#     queryset = Reviews.objects.all()
+#     serializer_class = ReviewsSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 class WatchListAPI(APIView):
